9b87344c2acf5b41d4755577f7d7d2ac486ddc14
Jerico Alcaras <jcalcaras@gmail.com>
2020-10-24 00:00:18 +0100

    Fixes.

--- cypress/integration/access-groups.spec.js
+++ cypress/integration/access-groups.spec.js
@@ -1,7 +1,6 @@
 describe('Access groups page spec', () => {
   it('Should show access groups page when left nav option is clicked', () => {
     cy.get('#left-nav-groups', { timeout: 10000 }).click();
-    cy.get('#permissions-menu').click();
     cy.get('.pal--page-header__title-text').should('be.equal', 'Access groups');
   });
 });
